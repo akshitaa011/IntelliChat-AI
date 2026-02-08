1b6b0e6279a2631ec142759abb7651172201b553
Akshita Singhal <akshitasinghal300@gmail.com>
2026-02-09 01:06:50 +0530

    Delete backend directory

--- backend/routes/generationRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateImage } = require('../controllers/imageGenerationController');
-
-router.post('/image', generateImage);
-
-module.exports = router;
--- backend/routes/imageRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { analyzeImage } = require('../controllers/imageAnalysisController');
-
-router.post('/analyze', analyzeImage);
-
-module.exports = router;
